e49c8a8183fc741e9307ecab00238679e59604c9
Andrew Shull <andrew.shull1@gmail.com>
2012-06-09 14:34:33 -0400

    Added task switching and task signaling timer ISRs.

--- test/test2.cpp
+++ test/test2.cpp
@@ -1,25 +1,44 @@
 #include "Arduino.h"
 
-int i = 0;
+volatile int i = 0;
 
 ISR(TIMER1_OVF_vect) {
-    TCNT1=0x0BDC;
-    i++;  
+    TCNT1=0xFDF8; 
 }
+
+
+ISR(TIMER3_OVF_vect) {
+    TCNT3=0xF9C0; // Initialize timer overflow reg to give 100 us interrupts
+    i++;
+}
+
 
 void setup()
 {
+    // Set up Timer1 Interrupts (Task timer)
     TIMSK1=0x01; // enabled global and timer overflow interrupt;
     TCCR1A = 0x00; // normal operation page 148 (mode0);
-    TCNT1=0x0BDC; // set initial value to remove time error (16bit counter register)
-    TCCR1B = 0x04; //
+    TCNT1=0xFDF8; // Initialize timer overflow reg to give 10 ms interrupts
+    TCCR1B = 0x04; // Prescale clock to system/256 (at 16 MHz = 62.5 kHz)
+
+    // Set up Timer3 Interrupts (Task switching)
+    TIMSK3=0x01; // enabled global and timer overflow interrupt;
+    TCCR3A = 0x00; // normal operation page 148 (mode0);
+    TCNT3=0xF9C0; // Initialize timer overflow reg to give 100 us interrupts
+    TCCR3B = 0x01; // No clock prescale (16 MHz)
 
     Serial.begin(9600);
 }
     
 void loop()
 {
-    Serial.print(i);
+    Serial.print("Begin\n");
+    for(;;) {
+        if(i > 10000) {
+            i = 0;
+            Serial.print("1 second\n");
+        }
+    }
 }
 /*
 int main (int argc, char const* argv[])
